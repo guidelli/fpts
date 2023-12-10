d86e6c385351d662031aac98b925f842d90eeabd
guidelli <agat10680@gmail.com>
2023-12-10 20:29:28 +0100

    c

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-import { parseUrl} from 'parse-url'
+import parseUrl  from 'parse-url'
 
 
 function isDotCom(url) {
 
 try{
-    const parsed= parseUrl('http://www.website.com/posts?hello=world')
+    const parsed= parseUrl(url)
+    const resource= parsed.resource
+    const parts= resource.split('.')
+    const n = parts.length
+
+    if(n>1){
+        const tld= parts[n-1]
+        if(tld==='com')
+         return true
+        return false
+    } else 
+    return false
 }
 catch{
     return false
 }
 
+
+
 }
+
+const log= console.log
+
+log(isDotCom('http://www.website.com/posts?hello=world'))
+
+log(isDotCom('http://www.website.net/posts?hello=world'))
+
+
+log(isDotCom('dqsdqs'))
