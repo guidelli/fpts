5ee57bece3fe4dfa279722a69e2d6d5601ddb4da
guidelli <agat10680@gmail.com>
2023-12-12 09:15:26 +0100

    c1

--- option/mapflattenchain.js
+++ option/mapflattenchain.js
@@ -2,6 +2,7 @@ import * as O from 'fp-ts/lib/Option.js'
 import { pipe } from 'fp-ts/lib/function.js'
 import { log, tapLog } from '../util.js'
 
+
 const foo= {
     bar: 'baz'
 }
@@ -13,33 +14,45 @@ const foobuzz= {
 }
 
 
-const map1= o=>pipe(o, 
+const mapbar= o=>pipe(o, 
     O.fromNullable, 
     O.map(({bar})=>bar))
 
 
 
-const map2= o=>pipe(o, 
+const mapbargetorelse= o=>pipe(o, 
     O.fromNullable, 
     O.map(({bar})=>bar), 
     O.getOrElse(()=>null))
 
 
-const map3= o=>pipe(o, 
+const mapbarbuzz= o=>pipe(o, 
     O.fromNullable, 
     O.map(({bar: {buzz}})=>buzz))
 
 
-log('\n--- map')
-log(map1(foo))
-log(map1(null))
 
-log('\n--- getOrElse')
-log(map2(foo))
-log(map2(null))
 
-log('\n--- flatten')
-log(map3(foobuzz))
-log(map3(null))
-log(map3(foo))
-log(map3({s: 55}))
+log('\n--- mapbar')
+log(mapbar(foo))
+log(mapbar(null))
+log(mapbar(55))
+
+
+log('\n--- mapbargetorelse')
+log(mapbargetorelse(foo))
+log(mapbargetorelse(null))
+log(mapbargetorelse(55))
+
+
+log('\n--- foobuzz')
+log(mapbarbuzz(foobuzz))
+log(mapbarbuzz(null))
+log(mapbarbuzz(foo))
+log((()=>{
+    try {
+        mapbarbuzz({s: 55})
+    } catch (error) {
+        return error.message
+    }
+})())
